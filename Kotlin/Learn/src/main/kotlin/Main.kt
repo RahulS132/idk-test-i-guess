e771225f21846463aa651a2dd8f722c5bcb561c9
Rahul Sigdel <rahulsigdel132@gmail.com>
2022-05-24 18:50:02 +0545

    nnothing new here

---
+++ Kotlin/Learn/src/main/kotlin/Main.kt
@@ -0,0 +1,12 @@
+fun main() {
+    println("Hello World!")
+    val a = 77 + 33
+    println(a)
+    val b =  10
+    val c = a + b + 30
+    val str = c.toString()
+    print(c)
+    val d = c + b
+    println(d)
+
+}
