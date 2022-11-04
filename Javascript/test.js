8e6a10c29caba24a3635521d1ed057458d4cf6cb
Rahul Sigdel <rahulsigdel132@gmail.com>
2022-11-04 17:05:19 +0545

    lul

--- Javascript/test.js
+++ Javascript/test.js
@@ -1,6 +1,9 @@
 // idk what im doin
 
-console.log("hello")
-var num = 5
-const pi = 3.14
-console.log(pi)
+console.log("hello");
+var a;
+var num = 5;
+const pi = 3.14;
+console.log(pi);
+a = ++num;
+console.log(a);
